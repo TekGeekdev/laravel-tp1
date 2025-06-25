77151f3daac18f17adb9f5dc9ea0afa0a3c943e2
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-06-24 22:52:39 -0400

    add configuration model with encode/decode json

---
+++ app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'content'];
+
+    public function student()
+{
+    return $this->belongsTo(Student::class);
+}
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => json_decode($value, true),
+            set: fn($value) => json_encode($value)
+        );
+    }
+
+    protected function content(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => json_decode($value, true),
+            set: fn($value) => json_encode($value)
+        );
+    }
+}
