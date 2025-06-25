f698412271557beb686874fe85ee10b28e416e63
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-06-25 16:25:49 -0400

    implement PostResources

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use App\Http\Resources\PostResource;
+
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content'];
+    protected $fillable = ['title', 'content','student_id'];
 
     public function student()
 {
@@ -29,5 +32,11 @@ class Post extends Model
             get: fn($value) => json_decode($value, true),
             set: fn($value) => json_encode($value)
         );
+    }
+
+    static public function posts(){
+        $post = PostResource::collection(self::orderBy('created_at', 'desc')->get())->resolve();
+        $sorted = collect($post)->sortBy('post')->values();
+        return $sorted->all();
     }
 }
